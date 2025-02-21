d25b702e9720029518c50258f8121b8e8cb8dbee
Kumar-vishal00021 <vishalkumar@Vishals-MacBook-Air.local>
2025-02-21 12:20:12 +0530

    API change

--- const.js
+++ const.js
@@ -1,5 +1,7 @@
-export const chartAPI = "https://stocks3.onrender.com/api/stocks/getstocksdata";
-export const stockListAPI =
-  "https://stocks3.onrender.com/api/stocks/getstockstatsdata";
-export const detailsAPI =
-  "https://stocks3.onrender.com/api/stocks/getstocksprofiledata";
+export const chartAPI = "https://stocksapi-uhe1.onrender.com/api/stocks/getstocksdata";
+export const stockListAPI = "https://stocksapi-uhe1.onrender.com/api/stocks/getstockstatsdata";
+export const detailsAPI = "https://stocksapi-uhe1.onrender.com/api/stocks/getstocksprofiledata";
+
+// export const chartAPI = "https://stocks3.onrender.com/api/stocks/getstocksdata";
+// export const stockListAPI = "https://stocks3.onrender.com/api/stocks/getstockstatsdata";
+// export const detailsAPI = "https://stocks3.onrender.com/api/stocks/getstocksprofiledata";
